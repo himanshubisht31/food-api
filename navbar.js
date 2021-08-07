567b1ffa53fca92dd96c6eba0b818250b8121cad
Himanshu <himanshubisht3112@gmail.com>
2021-08-07 23:17:13 +0530

    food api fetch

---
+++ navbar.js
@@ -0,0 +1,11 @@
+function navbar() {
+
+    return `<div id="navbar">
+        <a href="latest.html"><p>Latest Receipe</p></a>
+        <a href="receipe.html"><p>Receipe of the Day</p></a>
+        <a href="food.html"><p>Home</p></a>
+    </div>`;
+
+}
+
+export default navbar;
